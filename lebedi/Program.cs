a3df9186dc0b1ee47190d15e6e334f1c791e31ed
Евгений Маслов <evgeniy.maslov@etalongroup.com>
2023-05-21 01:40:21 +0300

    Test commit 2

--- lebedi/Program.cs
+++ lebedi/Program.cs
@@ -17,6 +17,7 @@ namespace lebedi
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Start_form());
+            //test
         }
     }
 }
